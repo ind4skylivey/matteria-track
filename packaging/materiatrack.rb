04eb60c79dc0a765803ca88623fdf4c713c1959d
il1v3y <il1v3y@users.noreply.github.com>
2025-11-27 22:00:48 +0000

    [DOCS] Packaging for v1.0.4 (AUR/Homebrew)

--- packaging/materiatrack.rb
+++ packaging/materiatrack.rb
@@ -4,8 +4,8 @@
 class Materiatrack < Formula
   desc "Mystical Final Fantasy-themed CLI time tracker based on Zeit"
   homepage "https://github.com/ind4skylivey/matteria-track"
-  url "https://github.com/ind4skylivey/matteria-track/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "8a51681613fcb885868d9e0c35c321c29c5660d0be2cc15811a4de6509874327"
+  url "https://github.com/ind4skylivey/matteria-track/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "92cb8bf4f1a6073bff61839cd146208417541ec4f5673cb18482cc4b3e5dbc37"
   license "MIT"
   head "https://github.com/ind4skylivey/matteria-track.git", branch: "main"
 
